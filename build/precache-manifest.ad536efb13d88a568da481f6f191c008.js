3c7b1d10f94938df022950d0a10b9ea039df9b4b
yanivakiva <yanivakiva20@gmail.com>
2022-08-01 08:25:13 +0300

    remove build

---
+++ build/precache-manifest.ad536efb13d88a568da481f6f191c008.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987c67451c8b89b46e8c47cfb43ddd7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "429a48f3c826aa3af6a6",
+    "url": "/static/css/2.1ec672bd.chunk.css"
+  },
+  {
+    "revision": "747f647fc15f2222e574",
+    "url": "/static/css/main.8e81dda7.chunk.css"
+  },
+  {
+    "revision": "429a48f3c826aa3af6a6",
+    "url": "/static/js/2.45255558.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.45255558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747f647fc15f2222e574",
+    "url": "/static/js/main.d1ecab55.chunk.js"
+  },
+  {
+    "revision": "202cb9d235c98c81f3bf",
+    "url": "/static/js/runtime-main.f8967765.js"
+  },
+  {
+    "revision": "9d122eb2a3b27fd1b8fef0ef21a39870",
+    "url": "/static/media/me.9d122eb2.jpg"
+  }
+]);
